f535d3f58497f792e64bab4bc2e1239d5cf00c42
Franck Nijhof <frenck@addons.community>
2019-03-23 19:06:04 +0100

    :hammer: Rewrite add-on onto Bashio

--- happy-bubbles/rootfs/etc/cont-init.d/11-nginx.sh
+++ happy-bubbles/rootfs/etc/cont-init.d/11-nginx.sh
@@ -1,25 +1,18 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Happy Bubbles
 # Configures NGINX for use with the Happy Bubbles presence server
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
-
 declare certfile
 declare keyfile
 
-if hass.config.true 'ssl'; then
+if bashio::config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
-    certfile=$(hass.config.get 'certfile')
-    keyfile=$(hass.config.get 'keyfile')
+    certfile=$(bashio::config 'certfile')
+    keyfile=$(bashio::config 'keyfile')
 
     sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
     sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
-fi
-
-if ! hass.config.true 'ipv6'; then
-    sed -i '/listen \[::\].*/ d' /etc/nginx/nginx.conf
 fi
